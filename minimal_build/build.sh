32fe83e6bbaa35279cf923caa907987c560026b0
John Sonchack <jsonch@princeton.edu>
2025-11-09 14:36:49 -0500

    cleanup

--- minimal_build/build.sh
+++
@@ -1,7 +0,0 @@
-gcc simple_rte_flow_rule.c -o simple_rte_flow_rule \
-	-I/opt/mellanox/dpdk/include/aarch64-linux-gnu/dpdk \
-	-I/opt/mellanox/dpdk/include/dpdk \
-	-I/opt/mellanox/doca/include/ \
-	-L/opt/mellanox/dpdk/lib/aarch64-linux-gnu \
-	-lrte_eal -lrte_mempool -lrte_ring -lrte_ethdev -lrte_mbuf \
-	-lstdc++ -libverbs -lmlx5
